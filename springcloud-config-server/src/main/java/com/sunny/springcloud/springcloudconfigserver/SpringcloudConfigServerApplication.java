d0177fd6847c2ff8b04126d120cf5e15824f79ef
sunny <1948904329@qq.com>
2018-12-12 17:31:21 +0800

    修改pom

--- springcloud-config-server/src/main/java/com/sunny/springcloud/springcloudconfigserver/SpringcloudConfigServerApplication.java
+++ springcloud-config-server/src/main/java/com/sunny/springcloud/springcloudconfigserver/SpringcloudConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.sunny.springcloud.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication(scanBasePackages = {"com.sunny.springcloud"})
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SpringcloudConfigServerApplication {
 
     public static void main(String[] args) {
