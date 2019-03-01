cd1830b642adeef6f96a64e654b8cbc239887ea1
sunny <1948904329@qq.com>
2019-03-01 15:14:37 +0800

    add

---
+++ springcloud-service-gateway-filter/src/main/java/com/sunny/springcloud/springcloudservicegatewayfilter/SpringcloudServiceGatewayFilterApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.springcloud.springcloudservicegatewayfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.sunny.springcloud"})
+public class SpringcloudServiceGatewayFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudServiceGatewayFilterApplication.class, args);
+    }
+
+}
